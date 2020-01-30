f1058f677f35d64f69b63227dc483ee8eb1400c7
Dhaval Patel <dhavalpatel@Dhavals-MacBook-Pro.local>
2020-01-30 13:48:55 -0500

    Updates

---
+++ precache-manifest.8c16e559a4244ef8bbfdbba21d26db6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c70ccb5b25de4a160b2d765fb37d3f7",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "9e4e4b1c0b4b3970e14a",
+    "url": "/react-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e3b2df6bf43c8abe2fd3",
+    "url": "/react-deploy/static/js/2.7f38fdf4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-deploy/static/js/2.7f38fdf4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9e4e4b1c0b4b3970e14a",
+    "url": "/react-deploy/static/js/main.067a6bd0.chunk.js"
+  },
+  {
+    "revision": "2a49018d5d1f167a6be3",
+    "url": "/react-deploy/static/js/runtime-main.8751e28a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
